738a81157ed55b0053f1c7b3ce76830e7ef7d864
teisen <teisen@mit.edu>
2018-09-14 18:53:06 -0400

    dephase script outputs fastq1 (dephased) and fastq2, but it's pretty slow

--- tail-seq-scripts/dephase.py
+++ tail-seq-scripts/dephase.py
@@ -12,6 +12,10 @@ from gzip import open as gzopen
 
 fastq1TAR=tarfile.open(name=sys.argv[1], mode='r:gz')
 fastq1open=fastq1TAR.extractfile(fastq1TAR.next())
+
+fastq2TAR=tarfile.open(name=sys.argv[2], mode='r:gz')
+fastq2open=fastq1TAR.extractfile(fastq1TAR.next())
+
 counter = 0
 
 dictPhase = {'objPhase1':re.compile("A..."),'objPhase2':re.compile("GA.."),'objPhase3':re.compile("CGA."),'objPhase4':re.compile("TCGA")}
@@ -38,7 +42,11 @@ def trim_adaptors(records, dictPhase, dictTrimPhase):
 originalReads = SeqIO.parse(fastq1open, "fastq")
 trimmedReads = trim_adaptors(originalReads,dictPhase,dictTrimPhase)
 #Write reads to second command line input was gz file. 
-with bgzf.BgzfWriter(sys.argv[2], "wb") as outgz:
-    count = SeqIO.write(sequences=trimmedReads, handle=outgz, format="fastq")
+with bgzf.BgzfWriter(sys.argv[3], "wb") as outgz:
+    count1 = SeqIO.write(sequences=trimmedReads, handle=outgz, format="fastq")
 
-print "Total sequences: %s"%(count)
+with bgzf.BgzfWriter(sys.argv[4], "wb") as outgz:
+    count2 = SeqIO.write(sequences=SeqIO.parse(fastq1open, "fastq"), handle=outgz, format="fastq")
+
+print "Total sequences, fastq file 1: %s"%(count1)
+print "Total sequences, fastq file 2: %s"%(count2)
