68b15d96e4192edb574db1a5606f27ca9617280a
teisen <teisen@mit.edu>
2019-07-28 17:07:13 -0400

    created a new hybrid pipeline just changing preprocess helpers and makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+  
 .DEFAULT_GOAL := help
 
 # Generates a help message. Borrowed from https://github.com/pydanny/cookiecutter-djangopackage.
@@ -12,13 +13,17 @@ help: ## Display this help message
 
 DataType?=Tail-seq
 Phasing?=False
-clipNo?=0
+clip5pR1?=0
 
 #Parse the arguments for the datatype
 ###TJE 20181209 Need to change the lines concerning parsing of PALseq and Tail-seq data to give warnings and exit if requirements aren't met
 ifeq (${DataType}, PALseq)
 	strand=S
 	state=True
+	readCommand=tar xzfO #Note that this must be set along with the same argument in the config file.
+# 	readCommand=zcat #Note that this must be set along with the same argument in the config file.
+	clip5pR2=0
+
 else
 	strand=s
 	state=True
@@ -26,7 +31,7 @@ endif
 
 #Parse the arguments for the Phasing
 ifeq (${Phasing}, True)
-	clipNo=12
+	clip5pR1=12
 endif
 
 #NEED TO CHANGE TO .GZ, NOT .TAR.GZ
@@ -37,26 +42,54 @@ parseArgs:
 	@echo ${Phasing}
 
 align-to-genome: ## Align rest of reads to genome and intersect with gff file
-	STAR --genomeDir $(genomeDir) --outSAMtype BAM SortedByCoordinate --outSAMattributes NH HI AS nM jM --alignIntronMax 1 --runThreadN 24 --outFilterMultimapNmax 1 --clip5pNbases ${clipNo} --outFilterMismatchNoverLmax 0.04 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --readFilesCommand tar xzfO --outSJfilterReads Unique --readFilesIn $(fastq1) $ --outFileNamePrefix $(outdir)/STAR_ > $(outdir)/stdOut_logFile.txt
+	STAR --genomeDir $(genomeDir) --outSAMtype BAM SortedByCoordinate --outSAMattributes NH HI AS nM jM --alignIntronMax 1 --runThreadN 24 --outFilterMultimapNmax 1 --clip5pNbases ${clip5pR1} --outFilterMismatchNoverLmax 0.04 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --readFilesCommand $(readCommand) --outSJfilterReads Unique --readFilesIn $(fastq1) $ --outFileNamePrefix $(outdir)/STAR_ > $(outdir)/stdOut_logFile.txt
+	
+	#read2, only 50 nt, clipping the first 4 if direct ligation. 
+	STAR --genomeDir $(genomeDir) --outSAMtype BAM SortedByCoordinate --outSAMattributes NH HI AS nM jM --alignIntronMax 1 --runThreadN 24 --outFilterMultimapNmax 1 --clip5pNbases ${clip5pR2} --clip3pNbases 200 --outFilterMismatchNoverLmax 0.04 --outFilterIntronMotifs RemoveNoncanonicalUnannotated --readFilesCommand $(readCommand) --outSJfilterReads Unique --readFilesIn $(fastq2) --outFileNamePrefix $(outdir)/Read2STAR_ > $(outdir)/stdOut_logFile_read2.txt
 
 intersect-gff:
-	bedtools intersect -abam $(outdir)/STAR_Aligned.sortedByCoord.out.bam -b $(gff) -bed -wb -${strand} > $(outdir)/read1.bed
+	bedtools intersect -abam $(outdir)/STAR_Aligned.sortedByCoord.out.bam -b $(gff) -bed -wb -wa -${strand} > $(outdir)/read1.bed
+	bedtools intersect -abam $(outdir)/Read2STAR_Aligned.sortedByCoord.out.bam -b $(gff) -bed -wb -wa -${strand} > $(outdir)/read2.bed
+
+filter_bam:
+	## Implements a filtering step for the Read2STAR_Aligned.sortedByCoord.out.bam file.
+	cut -f 4,21 $(outdir)/read2.bed > $(outdir)/read2temp.bed
+	cut -f 4,21 $(outdir)/read1.bed | grep -Ff - $(outdir)/read2temp.bed | cut -f 1 > $(outdir)/read2filtered.bed
+
+	#get the bam file header. 
+	samtools view -H $(outdir)/Read2STAR_Aligned.sortedByCoord.out.bam > $(outdir)/Read2Filtered_STAR_Aligned.sortedByCoord.out.sam
+
+	#filter the bam file, add it to the sam file.
+	samtools view -h $(outdir)/Read2STAR_Aligned.sortedByCoord.out.bam | grep -Ff $(outdir)/read2filtered.bed >> $(outdir)/Read2Filtered_STAR_Aligned.sortedByCoord.out.sam
+
+	#convert back to bam.
+	samtools view -b $(outdir)/Read2Filtered_STAR_Aligned.sortedByCoord.out.sam > $(outdir)/Read2Filtered_STAR_Aligned.sortedByCoord.out.bam
 
 signal-from-raw: ## Extract intensities for mapping reads and calculate normalized T-signal
-	python tail-seq-scripts/get_signal_from_raw.py --f1 $(fastq1) --f2 $(fastq2) -i $(intensity) -s $(standard_file) -o $(outdir) --strand ${strand}
+	python3 /lab/solexa_bartel/teisen/RNAseq/Scripts/PALseqKlinMod/tail-seq-scripts/get_signal_from_raw.py --f1 $(fastq1) --f2 $(fastq2) -i $(intensity) -s $(standard_file) -o $(outdir) --strand ${strand}
 
 signal-plot: ## Plot T-signal density
-	python tail-seq-scripts/plot_t_signals.py -o $(outdir) -b 100
+	python3 /lab/solexa_bartel/teisen/RNAseq/Scripts/PALseqKlinMod/tail-seq-scripts/plot_t_signals.py -o $(outdir) -b 100
 
 tail-seq: ## Train and run HMM for calling tail lengths
-	python tail-seq-scripts/tail_length_hmm.py -o $(outdir) --twostate ${state} --futures 24
+	python3 /lab/solexa_bartel/teisen/RNAseq/Scripts/PALseqKlinMod/tail-seq-scripts/tail_length_hmm.py -o $(outdir) --twostate ${state}
 
 summary: ## Aggregate individual tail lengths by accession and plot standards
-	python tail-seq-scripts/summarize_results.py -o $(outdir)
+	python3 /lab/solexa_bartel/teisen/RNAseq/Scripts/PALseqKlinMod/tail-seq-scripts/summarize_results.py -o $(outdir)
 
-all: parseArgs align-to-genome intersect-gff signal-from-raw signal-plot tail-seq summary ## Run all at once
+clean:
+	rm $(outdir)/read2temp.bed
+	rm $(outdir)/read2filtered.bed
+	rm $(outdir)/Read2Filtered_STAR_Aligned.sortedByCoord.out.sam
 
-all_from_bam: intersect-gff signal-from-raw signal-plot tail-seq summary ## Run without STAR aligning
+
+all: parseArgs align-to-genome intersect-gff filter_bam signal-from-raw signal-plot tail-seq summary clean ## Run all at once
+
+all_no_parse: align-to-genome intersect-gff filter_bam signal-from-raw signal-plot tail-seq summary clean ## Run all at once
+
+all_from_bam: intersect-gff filter_bam signal-from-raw signal-plot tail-seq summary ## Run without STAR aligning
+
+all_from_bed: signal-from-raw signal-plot tail-seq summary ##Run without intersect
 
 all_hmm: tail-seq summary 
 
--- tail-seq-scripts/preprocess_helpers.py
+++ tail-seq-scripts/preprocess_helpers.py
@@ -6,7 +6,9 @@ import pandas as pd
 import regex
 
 import config
-
+from itertools import groupby
+import pysam
+import pdb
 
 def reverse_complement(seq):
     """Get reverse complement of sequence"""
@@ -19,7 +21,6 @@ def dedup_bed(bedfile):
     If a read maps to multiple genes, discard.
     If a read maps to multiple exons of a gene, use later exon.
     Extract 3' end of read. Note that reads are reverse complement of genes.
-
     Arguments:
         bedfile: pandas DataFrame of intersectBed columns [chr, start, end, read_ID, strand, gene]
     """
@@ -32,8 +33,8 @@ def dedup_bed(bedfile):
     minus.columns = columns
 
     # for reads that map to multiple exons of the same transcript, take later exon
-    plus_last_exon = plus.drop_duplicates(subset=['read_ID','accession'], keep='first')
-    minus_last_exon = minus.drop_duplicates(subset=['read_ID','accession'], keep='first')
+    plus = plus.drop_duplicates(subset=['read_ID','accession'], keep='first') #Previously, I think these reads were lost. TJE 2019 03 13
+    minus = minus.drop_duplicates(subset=['read_ID','accession'], keep='first')
 
     # discard reads that map to multiple genes
     plus = plus.drop_duplicates(subset=['read_ID'], keep=False)
@@ -47,10 +48,27 @@ def dedup_bed(bedfile):
 
     return reads_dedup, dropped_reads
 
+def parse_read2_BAM(outdir):
+    """Parse a BAM file for read2, returning a dictionary of soft clipping.
+    Arguments:
+        unopened outdir
+    """
+    sam = pysam.AlignmentFile(os.path.join(outdir, 'Read2Filtered_STAR_Aligned.sortedByCoord.out.bam'), "rb")
+    softClippingDict = {}
+    for read in sam:
+        read_ID = config.fastq_header_to_ID(read.query_name)
+        if read.flag & 16 == 16: 
+            # seqRaw = reverse_complement(read.seq) #extract the strand from the bitwise operator.
+            softClipping = read.cigartuples[-1][1] #cigar identifiers for soft clipping. 
+        else: 
+            # seqRaw =  read.seq
+            softClipping = read.cigartuples[0][1]
+        softClippingDict[read_ID] = softClipping
+
+    return(softClippingDict)
 
 def parse_read1(fastq1, keep_dict, standard_dict):
     """Parse fastq1 file, extract reads that match standard sequences.
-
     Arguments:
         fastq1: openened fastq1 file, from tarfile, with lines as binary. 
         keep_dict: dictionary of {read_ID: None} for reads that pass filters so far
@@ -90,68 +108,98 @@ def parse_read1(fastq1, keep_dict, standard_dict):
     return new_keep_dict, standard_reads
 
 
-def parse_read2(fastq2, keep_dict, outdir, qual_filter = True):
+def process_short(seq, softClipping):
+    """Parse a short-read sequence, taking into account the number of nucleotides of soft-clipping.
+    
+    Arguments:
+        sequence, softclipping number, already adjusted for the random nucleotides of the 3p adapter.
+    
+    Outputs the tail length    
+    """
+    firstBasesList = [[k, len(list(g))] for k, g in groupby(seq)] #list of tuples (nucleotide: count)
+    
+    if firstBasesList[0][0] == 'T': #read has a tail, begins with T
+        TL = firstBasesList[0][1]
+    
+    elif len(firstBasesList) == 1: TL = 0
+    
+    #allow G/C for fewer than 2 Gs or Cs or and As.
+    elif firstBasesList[1][0] == 'T' and (firstBasesList[0][0] == 'A' or firstBasesList[0][1] <= 2): 
+        #These next two lines require that at least all non-T nt prior to the T stretch be soft clipped
+        if softClipping >= firstBasesList[0][1]: TL = firstBasesList[1][1] #Just the number of Ts
+        else: TL = 0
+
+    else:
+        for i in range(19,7,-1):
+            query = ('T'*i)
+            #This next line requires that the read needs to be soft clipped by at least 3 nt (a 5 nt untemplated tail).
+            if query in seq[:30] and softClipping >= seq[:30].index(query) + 6:
+                TL = i
+                break
+        else: TL = 0
+   
+    return(TL)
+
+def parse_read2(fastq2, keep_dict, outdir, softClippingDict, standard_reads, qual_filter = True):
     """Parse fastq2 file, filter low quality or very short tails.
     Manually call tails between 4 and 9 nucleotides (inclusive).
-
     Arguments:
         fastq2: openened fastq2 file, from tarfile, with lines as binary. 
         keep_dict: dictionary of {read_ID: read1 sequence} for reads that pass filters so far
         outdir: path to output directory
-
     Modified by TJE on 20190125 to allow argument to turn off low quality filtering. 
     """
+
     new_keep_dict = {}
     short_tail_outfile = open(os.path.join(outdir, 'short_tails.txt'), 'w')
     short_tail_outfile.write('read_ID\ttail_length\n')
     num_short_tails = 0
     dropped_read2 = []
     line_counter = 0
-
-    if qual_filter: r = regex.compile('(%s){e<=1}' % 'TTTTTTTTTTT')
-    else: r = regex.compile('(%s){e<=2}' % 'TTTTTTTTTTT')
+    standards = set(standard_reads['read_ID'].unique())
+    total_lines = 0
+    strMove = 30 #What is the thershold for going into the HMM?
 
     for line in fastq2:
+        # Parse the fastq
         line = line.decode("utf-8")
+        total_lines += 1
+        # if total_lines % 1000 == 0: print("lines processed: %s"%(total_lines))
         if line_counter == 0:
             if line[0] != '@':
                 raise ValueError('Fastq2 file must begin with @')
-
             read_ID = config.fastq_header_to_ID(line[1:])
         elif line_counter == 1:
-            seq = line
-
-            if read_ID in keep_dict:
-
-                # pass if basecaller confused about more than 2 bases in first 25 nucleotides
-                if qual_filter and seq[:25].count('N') >= 2:
-                    dropped_read2.append([read_ID, 'low_qual_read2'])
-                
-                else:
-                    # look for at least 11 contiguous T's in first 30 nucleotides, allowing 1 error
-                    match = r.search(seq[:30]) #changes this to two mismatches.
-
-                    # if found, keep the ID and where the tail starts
-                    if match is not None:
-                        match_start = match.start()
-                        new_keep_dict[read_ID] = (keep_dict[read_ID], match_start)
-
-                    # otherwise manually call tail if read starts with >= 4 contiguous T's
-                    else:
-                        if seq[:4] == 'TTTT':
-                            TL = 4
-                            for nt in seq[4:10]:
-                                if nt == 'T':
-                                    TL += 1
-                                else:
-                                    break
-                            short_tail_outfile.write('{}\t{}\n'.format(read_ID, TL))
-                            num_short_tails += 1
-
-                        else:
-                            dropped_read2.append([read_ID, 'no_tail'])
-
+            seq = line[config.TRIM_BASES:] #In a splint run, this is 0. In a direct lig run, this is 4. 
         line_counter = (line_counter + 1) % 4
+        if line_counter != 0 or read_ID not in keep_dict: continue
+
+        # pass if basecaller confused about more than 2 bases in first 25 nucleotides
+        if qual_filter and seq[:25].count('N') >= 2:
+            dropped_read2.append([read_ID, 'low_qual_read2'])
+            continue
+
+        if read_ID not in softClippingDict: #Reads are called by the HMM, including standards.
+            r = regex.compile('(%s){e<=1}' % ('T'*12))
+            match = r.search(seq[:30])
+            if match is not None: #Where does the tail start?
+                match_start = match.start()
+                if seq[match_start] != 'T': match_start += 1 #if error is first pos.
+                if seq[match_start] != 'T': print("Error with starting position.")
+                match_start = match_start + config.TRIM_BASES 
+                new_keep_dict[read_ID] = (keep_dict[read_ID], match_start)
+
+            elif read_ID in standards: #mostly for the 10mer. 
+                new_keep_dict[read_ID] = (keep_dict[read_ID], config.TRIM_BASES)
+
+            else: dropped_read2.append([read_ID, 'no_tail_unmapped_read2']) #new designation as of 2019 06 18
+
+        else: #Call the tail manually
+            TL = process_short(seq,softClippingDict[read_ID] - config.TRIM_BASES)
+            if TL != 0: 
+                short_tail_outfile.write('{}\t{}\n'.format(read_ID, TL))
+                num_short_tails += 1
+            else: dropped_read2.append([read_ID, 'no_tail'])
 
     short_tail_outfile.close()
     return new_keep_dict, dropped_read2, num_short_tails
